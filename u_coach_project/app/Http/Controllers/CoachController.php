5e356e2e1947f5533913e164aea6135d735c55e8
Elgountari-Ayoub <elgountariayoub22@gmail.com>
2023-04-12 15:34:39 +0000

    setup the Coach mcr

---
+++ u_coach_project/app/Http/Controllers/CoachController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coach;
+use Illuminate\Http\Request;
+
+class CoachController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Coach $coach)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Coach $coach)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Coach $coach)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Coach $coach)
+    {
+        //
+    }
+}
